151cacc4642ce893be99c46898c2a5ad5f4c163e
Jason Shaner <jshaner@cnm.edu>
2020-06-17 16:13:14 -0600

    Add throwable observer.

--- app/src/main/java/edu/cnm/deepdive/quotes/controller/QuotesFragment.java
+++ app/src/main/java/edu/cnm/deepdive/quotes/controller/QuotesFragment.java
@@ -4,13 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import edu.cnm.deepdive.quotes.R;
+import edu.cnm.deepdive.quotes.model.entity.Quote;
 import edu.cnm.deepdive.quotes.viewmodel.MainViewModel;
+import java.lang.reflect.Array;
 
 public class QuotesFragment extends Fragment {
 
@@ -23,7 +26,9 @@ public class QuotesFragment extends Fragment {
     //noinspection ConstantConditions
     mainViewModel = new ViewModelProvider(getActivity()).get(MainViewModel.class);
     mainViewModel.getQuotes().observe(getViewLifecycleOwner(), (quotes) -> {
-      //TODO Populate adapter and attach to view.
+      ArrayAdapter<Quote> adapter =
+          new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, quotes);
+      quoteList.setAdapter(adapter);
     });
   }
 
